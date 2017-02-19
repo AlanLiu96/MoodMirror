7cd4cc253d1c1071dc1107319a5d2f15530cde78
Alan Liu <alanl.liu@yahoo.com>
2017-02-19 10:40:57 -0800

    disable debug

--- main.py
+++ main.py
@@ -259,4 +259,5 @@ def local_to_desiredTZ():
     return local_dt
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    # app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000)
